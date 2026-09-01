b32de86f60f114a66f70bae0dd420628a899f7ac
FabhianHarland <fabhianharlan12@gmail.com>
2026-09-01 10:15:40 +0700

    PrakAlpro_Pekan1

---
+++ Pekan1/Program1.py
@@ -0,0 +1,14 @@
+# Program pertama 
+"""
+Komentar 
+dua baris atau lebih 
+pertemuan 1 pratikum alpro
+"""
+
+print("Hello World")
+print()
+print("program ini menghasilkan")
+print("Empat baris output")
+print("/ \\ // \\\\ /// \\\\")
+print("program ini" + "dibuat oleh a\n" + "M.Fabhian Harland dan 2611532019")
+
